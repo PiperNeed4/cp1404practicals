d02d0cf29b2a23b243b354c30643a27d7058f843
Piper <ben.needham@my.jcu.edu.au>
2024-10-04 12:27:56 +1000

    Add randoms.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,6 @@
+import random
+# line 1: The number seen was 20, the minimum was 5, the maximum was 20
+# line 2: The number seen was 5, the minimum was 3, the maximum was 10,
+# it could not have produced a 4 as it can only generate increments of 2
+# line 3: the number seen was 3.4912435740216052, the minimum was 2.5, the maximum was 5.5
+print(random.uniform(1, 100))
