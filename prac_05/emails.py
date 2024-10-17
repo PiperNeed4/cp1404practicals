3424fe516b9ca2c005ea846b402d00cf6a2b6be1
piper <ben.needham@my.jcu.edu.au>
2024-10-17 14:26:36 +1000

    Add emails.py completed

---
+++ prac_05/emails.py
@@ -0,0 +1,24 @@
+"""
+Estimate 18 minutes
+Actual 23 minutes
+"""
+def main():
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = extract_name(email)
+        correction = input(f"Is your name {name}? (Y/n) ").upper()
+        if correction != "Y":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+def extract_name(email):
+    stripped_email = email.split('@')[0]
+    parts = stripped_email.split('.')
+    name = " ".join(parts).title()
+    return name
+
+main()
