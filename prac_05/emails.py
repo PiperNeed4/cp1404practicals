8072a864b4d0696171873aa258ca492855f07990
piper <ben.needham@my.jcu.edu.au>
2024-10-17 14:29:06 +1000

    Add doc strings to functions in emails.py

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,7 +2,10 @@
 Estimate 18 minutes
 Actual 23 minutes
 """
+
+
 def main():
+    """Get emails and store names in a dictionary."""
     email_to_name = {}
     email = input("Email: ")
     while email != "":
@@ -15,10 +18,13 @@ def main():
     for email, name in email_to_name.items():
         print(f"{name} ({email})")
 
+
 def extract_name(email):
+    """Extract potential name from provided email address."""
     stripped_email = email.split('@')[0]
     parts = stripped_email.split('.')
     name = " ".join(parts).title()
     return name
+
 
 main()
