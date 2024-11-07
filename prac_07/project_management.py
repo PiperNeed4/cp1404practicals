f753832e7cafb98f2baab3f4ea57724d9d8b7b24
piper <ben.needham@my.jcu.edu.au>
2024-11-07 14:47:08 +1000

    Add file reading to main

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,13 +1,22 @@
 """
 Estimate: 40 minutes
 Actual:
+current: 27
 """
-from prac_01.temperatures import choice
+from prac_07.project import Project
 
 MENU = ("- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ileter projects by date\n- (A)dd new project\n"
         "- (U)pdate project\n- (Q)uit")
 
 def main():
+    projects = []
+    in_file = open("projects.txt", 'r', encoding="utf-8-sig")
+    in_file.readline()
+    for line in in_file:
+        parts = line.strip().split('  ')
+        print(parts)
+        # projects.append(Project(parts[0], parts[1], parts[2], parts[3], parts[4]))
+    print(projects)
     choice = input(MENU).upper()
     while choice != "Q":
         if choice == "L":
@@ -25,3 +34,9 @@ def main():
         else:
             print("Invalid input")
             choice = input(MENU).upper()
+
+def display_projects():
+    pass
+
+
+main()
