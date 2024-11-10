884b37dd43b045aacae7922b3bf49d6fbeb01b1c
Piper <ben.needham@my.jcu.edu.au>
2024-11-10 17:21:41 +1000

    Add save functionality

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,10 +1,14 @@
+import datetime
 
 class Project:
 
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
 
         self.name = name
-        self.start_date = start_date
+        self.start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        return f"{self.name} {self.start_date} {self.priority} {self.cost_estimate} {self.completion_percentage}"
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,12 +1,13 @@
 """
 Estimate: 40 minutes
 Actual:
-current: 27
+current: 47
 """
 from prac_07.project import Project
 
-MENU = ("- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ileter projects by date\n- (A)dd new project\n"
-        "- (U)pdate project\n- (Q)uit")
+MENU = ("- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new project\n"
+        "- (U)pdate project\n- (Q)uit\n >>> ")
+
 
 def main():
     projects = []
@@ -15,21 +16,23 @@ def main():
     for line in in_file:
         parts = line.strip().split('  ')
         print(parts)
-        # projects.append(Project(parts[0], parts[1], parts[2], parts[3], parts[4]))
+        projects.append(Project(parts[0], parts[1], parts[2], parts[3], parts[4]))
     print(projects)
     choice = input(MENU).upper()
     while choice != "Q":
         if choice == "L":
-            load_objects()
+            load_projects(projects)
             choice = input(MENU).upper()
         elif choice == "S":
-            pass
+            save_projects(projects)
+            choice = input(MENU).upper()
         elif choice == "D":
-            pass
+            display_projects(projects)
+            choice = input(MENU).upper()
         elif choice == "F":
             pass
         elif choice == "A":
-            add_project()
+            add_project(projects)
             choice = input(MENU).upper()
         elif choice == "U":
             pass
@@ -37,31 +40,35 @@ def main():
             print("Invalid input")
             choice = input(MENU).upper()
 
-def load_objects():
+
+def load_projects(projects):
     filename = input("Enter file name: ")
     in_file = open(filename, 'r', encoding="utf-8-sig")
     in_file.readline()
     for line in in_file:
         parts = line.strip().split('  ')
-        print(parts)
-        # projects.append(Project(parts[0], parts[1], parts[2], parts[3], parts[4]))
-
-# def save_objects():
+        projects.append(Project(parts[0], parts[1], parts[2], parts[3], parts[4]))
 
 
+def save_projects(projects):
+    filename = input("Enter file name: ")
+    out_file = open(filename, 'w')
+    for project in projects:
+        print(project, sep="", file=out_file)
 
-def add_project():
+
+def add_project(projects):
     print("Let's add a new project")
     name = input("Name: ")
-    start_date = input("Start date: ")
-    priority = input("Priority (dd/mm/yy): ")
+    start_date = input("Start date (dd/mm/yy): ")
+    priority = input("Priority: ")
     cost_estimate = input("Cost estimate: $")
     completion_percentage = input("Percent complete: ")
-    # projects.append(Project(name, start_date, priority, cost_estimate, completion_percentage))
+    projects.append(Project(name, start_date, priority, cost_estimate, completion_percentage))
 
 
-def display_projects():
-    pass
+def display_projects(projects):
+    print(projects)
 
 
 main()
