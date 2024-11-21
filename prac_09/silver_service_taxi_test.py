d2b946f0cc6dbaa1a78af4498d407e9c4d24515c
piper <ben.needham@my.jcu.edu.au>
2024-11-21 15:35:21 +1000

    Remove Prac09 work

--- prac_09/silver_service_taxi_test.py
+++
@@ -1,6 +0,0 @@
-from prac_09.silver_service_taxi import SilverServiceTaxi
-
-my_taxi = SilverServiceTaxi("Hummer", 18, 2)
-my_taxi.drive(40)
-print(f"${my_taxi.get_fare()}" )
-print(my_taxi.__str__())
