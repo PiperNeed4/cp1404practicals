e4bdde8ba19bed80f95e01b0314fe876a9a0d856
piper <ben.needham@my.jcu.edu.au>
2024-11-21 13:24:38 +1000

    Complete taxi_test.py tasks

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,10 @@
+from prac_09.car import Car
+from prac_09.taxi import Taxi
+
+my_taxi = Taxi("Prius 1", 100, 1.23)
+my_taxi.drive(40)
+print(my_taxi.__str__())
+my_taxi.start_fare()
+my_taxi.drive(100)
+print(my_taxi.__str__())
+
